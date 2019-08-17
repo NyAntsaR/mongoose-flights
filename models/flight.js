9258e1669a5678b5fc8bdd3179d7e8f0247a71a3
NyAntsa <nyantsa.rajaonarison@gmail.com>
2019-08-16 18:49:18 -0700

    Mongoose-flight lab part 2

---
+++ models/flight.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var destinationSchema = new Schema ({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SEA']
+    },
+    arrival: Date
+});
+
+var flightSchema = new Schema ({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+
+    departs: {
+        type: Date,
+        default: new Date(new Date().setFullYear(new Date().getFullYear() +1))
+    },
+
+    airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SEA'],
+        default: 'SEA'
+    },
+    
+    destinations: [destinationSchema]
+    },
+    
+    {
+        timestamps: true
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
